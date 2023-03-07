0b133937d7daf674d0b7f315e1ccb245e481e7ab
Sandro Livino <sandro.livino@gmail.com>
2023-03-07 15:42:53 -0300

    Conversores e validadores de formulários jsf

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -1,9 +1,11 @@
 package com.algaworks.erp.controller;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.Column;
 
 import com.algaworks.erp.model.Empresa;
 import com.algaworks.erp.model.TipoEmpresa;
@@ -16,7 +18,24 @@ public class GestaoEmpresasBean implements Serializable {
     
     private Empresa empresa = new Empresa();
     
-    public void salvar() {
+    @Column(precision = 10, scale = 2)
+    private BigDecimal faturamento;
+    
+
+	public BigDecimal getFaturamento() {
+		return faturamento;
+	}
+	
+	public void setFaturamento(BigDecimal faturamento) {
+		this.faturamento = faturamento;
+	}
+	
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+
+	public void salvar() {
         System.out.println("Razão social: " + empresa.getRazaoSocial()
                 + " - Nome fantasia: " + empresa.getNomeFantasia()
                 + " - Tipo: " + empresa.getTipo());
