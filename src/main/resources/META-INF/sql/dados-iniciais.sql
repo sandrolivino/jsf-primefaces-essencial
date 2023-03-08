1ba77e80941607df4c7424932641a961d1d35a1e
Sandro Livino <sandro.livino@gmail.com>
2023-03-08 20:38:14 -0300

    Inseridos mais dados no arquivo dados-iniciais.sql

--- src/main/resources/META-INF/sql/dados-iniciais.sql
+++ src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -13,3 +13,7 @@ insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao,
 insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (2, '52.822.994/0001-25', 'Fale Mais', 'Fale Mais Telecom S.A.', 'SA', '1997-12-10', 2);
 insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (3, '41.952.519/0001-57', 'Maria de Souza da Silva', 'Maria de Souza da Silva', 'MEI', '2014-10-15', 3);
 insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (4, '16.134.777/0001-89', 'Gomes Inovação', 'José Fernando Gomes EIRELI ME', 'EIRELI', '2009-03-02', 4);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (5, '45.185.196/0001-74', 'Casa de Carnes Ribeiro', 'Silvio Santos Ltda', 'LTDA', '2009-06-02', 1);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (6, '48.415.141/0001-00', 'Guarapari Supermercados', 'Guarapari S.A.', 'SA', '1997-01-10', 2);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (7, '88.356.448/0001-93', 'Veneza Supermercados', 'Rita Cadillac', 'MEI', '2014-12-15', 3);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (8, '73.027.487/0001-70', 'Barber"s Barbearia', 'Cigano EIRELI ME', 'EIRELI', '2009-09-02', 4);
